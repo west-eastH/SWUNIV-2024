819dd847b437614edab0346838c5f3493b2bc3d5
mjmj1 <mjong3365@gmail.com>
2024-10-07 14:02:58 +0900

    [test]:pull request testing

--- hanbatbox-api/src/main/java/com/hbu/hanbatbox/HanbatboxApplication.java
+++ hanbatbox-api/src/main/java/com/hbu/hanbatbox/HanbatboxApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HanbatboxApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hanbatbox Application Started");
 		SpringApplication.run(HanbatboxApplication.class, args);
 	}
 
