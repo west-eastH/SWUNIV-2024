2d4679adf24c1044b7805d3c99afc275b2b7ee73
galaxy4276 <deveungi@gmail.com>
2024-10-09 16:12:04 +0900

    [doc]: add PR Template

---
+++ app/tailwind.config.ts
@@ -0,0 +1,16 @@
+import { palette } from "./src/shared/style/consts/palette";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: palette,
+    },
+  },
+  plugins: [],
+}
+
