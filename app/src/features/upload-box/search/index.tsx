0ee0d9bf1ee7b2477a0af2b47d1b9ee76b704350
galaxy4276 <deveungi@gmail.com>
2024-10-13 00:33:37 +0900

    feat: add search part about upload

---
+++ app/src/features/upload-box/search/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Dropdown, Input } from "@shared/ui";
+import clsx from "clsx";
+
+type Props = {} & Pick<HTMLElement, "className">;
+enum DropdownOptionKey {
+  FILENAME = "filename",
+  UPLOADER = "uploader",
+}
+
+const dropdownOptions = [
+  { value: DropdownOptionKey.FILENAME, label: "파일명" },
+  { value: DropdownOptionKey.UPLOADER, label: "닉네임" },
+];
+
+export const Search: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={clsx([
+      'mt-[18px] mb-[24px]',
+      'flex gap-x-3 w-full',
+      className,
+    ])}>
+      <Input placeholder="기업과 창업.pdf" className="w-full" />
+      <Dropdown
+        options={dropdownOptions}
+        placeholder="구분"
+        className="h-[46px] w-[120px]"
+        onChange={console.log}
+      />
+    </div>
+  );
+};
