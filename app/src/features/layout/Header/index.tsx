0152e3c98ba82c8685ca0981e03c78eabfab3041
galaxy4276 <deveungi@gmail.com>
2024-10-12 11:01:58 +0900

    feat: add mobile layout

---
+++ app/src/features/layout/Header/index.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from 'react';
+import { Icon } from "@shared/ui";
+
+export type HeaderType = "header-logo";
+
+type Props = { children: HeaderType | ReactElement; }
+
+const headerMap: Record<HeaderType, ReactElement> = {
+  "header-logo": <Icon.HeaderLogo />,
+};
+
+const Header: React.FC<Props> = ({ children }) => {
+  const isHeaderType = typeof children === "string";
+  const header = headerMap[children as HeaderType];
+
+  return (
+    <header className="h-[56px] flex justify-center items-center border-b border-[#F4F4F4]">
+      {isHeaderType && header}
+      {!isHeaderType && children}
+    </header>
+  );
+}
+
+export default Header;
+
