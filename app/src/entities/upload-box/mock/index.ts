f91c386f3d0d24ff47d7d48885ada836d77ed9c8
galaxy4276 <deveungi@gmail.com>
2024-10-13 12:56:16 +0900

    feat: add upload-box mock data

---
+++ app/src/entities/upload-box/mock/index.ts
@@ -0,0 +1,10 @@
+import { UploadBoxDetails } from "@entities/upload-box";
+
+export const mockUploadBox: UploadBoxDetails = {
+  uploader: "창의적인 냥냥이",
+  id: 3,
+  dateUploaded: "2024-10-13 14:56:52",
+  title: "기업과 창업.pdf",
+  crypted: false,
+  fileSize: 59448,
+};
