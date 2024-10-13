05c330eb5acd76804edd2f0181d84da7bf309b0c
west_east <west.east1832@gmail.com>
2024-10-13 16:54:14 +0900

    [fix]: feat IP Check Controller

--- hanbatbox-api/src/main/java/com/hbu/hanbatbox/controller/IpCheckController.java
+++ hanbatbox-api/src/main/java/com/hbu/hanbatbox/controller/IpCheckController.java
@@ -7,21 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IpCheckController {
+
+    public record AccessAllowed(boolean accessible) {}
+
     private boolean isIpAllowed(HttpServletRequest request) {
         String clientIp = request.getRemoteAddr();
-
         return clientIp.startsWith("223.194.160.") || clientIp.equals("127.0.0.1")
             || clientIp.equals("::1") || clientIp.equals("0:0:0:0:0:0:0:1");
     }
 
     @GetMapping("/check-ip")
-    public ResponseEntity<String> checkClientIp(HttpServletRequest request) {
-        String clientIp = request.getRemoteAddr();
-
-        if (isIpAllowed(request)) {
-            return ResponseEntity.ok("Allowed IP: " + clientIp);
-        } else {
-            return ResponseEntity.status(400).body("The IP address is not allowed.");
-        }
+    public ResponseEntity<AccessAllowed> checkClientIp(HttpServletRequest request) {
+        boolean isAllowed = isIpAllowed(request);
+        return ResponseEntity.ok(new AccessAllowed(isAllowed));
     }
 }
