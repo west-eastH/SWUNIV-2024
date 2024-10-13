328036b4aaa34ad05760e34d6038eb37fe140608
galaxy4276 <deveungi@gmail.com>
2024-10-13 12:57:40 +0900

    [feat]: add upload-box card component

---
+++ app/src/shared/ui/card/index.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode, DetailedHTMLProps, HTMLAttributes } from 'react';
+import clsx from "clsx";
+
+type Props = { children: ReactNode; } & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+export const Card: React.FC<Props> = ({ children, className, ...props }) => {
+  return (
+    <article className={clsx([
+      "w-full border border-1 border-[#E3E3E3] drop-shadow-sm rounded-[12px]",
+      className,
+    ])}>
+      {children}
+    </article>
+  );
+};
