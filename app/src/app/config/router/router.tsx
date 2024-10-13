3c32ce7969d77f6c61be29dfd09e26724847655c
galaxy4276 <deveungi@gmail.com>
2024-10-13 16:59:43 +0900

    [publish]: add upload page

--- app/src/app/config/router/router.tsx
+++ app/src/app/config/router/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { urlPath } from "./consts/path";
 import { MainRouter } from "@pages/landing";
+import UploadPage from "@pages/upload/ui/page";
 
 export const router = createBrowserRouter([
   {
     path: urlPath.root,
     element: <MainRouter />,
+  },
+  {
+    path: urlPath.upload,
+    element: <UploadPage />,
   },
 ]);
