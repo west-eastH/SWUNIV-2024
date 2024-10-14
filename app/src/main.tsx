955caa1d8716f32d322c26eb9515089cf9a0cdc0
galaxy4276 <deveungi@gmail.com>
2024-10-14 09:16:51 +0900

    [style]: adjust some styles

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { Outlet } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Outlet />
     <App />
   </StrictMode>,
 );
