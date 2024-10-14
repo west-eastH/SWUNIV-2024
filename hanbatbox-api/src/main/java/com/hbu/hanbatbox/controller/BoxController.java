a3407bd0dfb7eb00266d5f9f30f0ed261300963d
galaxy4276 <deveungi@gmail.com>
2024-10-15 00:51:57 +0900

    [fix]: change dto

--- hanbatbox-api/src/main/java/com/hbu/hanbatbox/controller/BoxController.java
+++ hanbatbox-api/src/main/java/com/hbu/hanbatbox/controller/BoxController.java
@@ -1,39 +1,40 @@
 package com.hbu.hanbatbox.controller;
 
+import com.hbu.hanbatbox.controller.annotation.MultipartMapping;
+import com.hbu.hanbatbox.controller.dto.BoxCreation;
 import com.hbu.hanbatbox.dto.BoxGetDto;
-import com.hbu.hanbatbox.dto.BoxSaveDto;
 import com.hbu.hanbatbox.dto.Result;
 import com.hbu.hanbatbox.service.BoxService;
-import jakarta.servlet.http.HttpServletRequest;
+
 import java.util.List;
+
+import com.hbu.hanbatbox.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/boxes")
 public class BoxController {
 
-    private final BoxService boxService;
+  private final BoxService boxService;
+  private final S3Service s3Service;
 
-    @GetMapping("/boxes")
-    public ResponseEntity<Result<List<BoxGetDto>>> getAllBoxes(
-        @RequestParam(required = false) Long cursor,
-        @RequestParam(required = false) String keyword,
-        @RequestParam(required = false) String type) {
+  @GetMapping
+  public ResponseEntity<Result<List<BoxGetDto>>> getAllBoxes(
+      @RequestParam(required = false) Long cursor,
+      @RequestParam(required = false) String keyword,
+      @RequestParam(required = false) String type) {
 
-        List<BoxGetDto> boxes = boxService.searchBoxes(keyword, type, cursor);
-        return ResponseEntity.ok(new Result<>(200, "Success", boxes));
-    }
+    List<BoxGetDto> boxes = boxService.searchBoxes(keyword, type, cursor);
+    return ResponseEntity.ok(new Result<>(200, "Success", boxes));
+  }
 
-    @PostMapping("/boxes")
-    public ResponseEntity<String> createBox(@RequestBody BoxSaveDto boxDto) {
-
-        boxService.saveBox(boxDto);
-        return ResponseEntity.ok("Box created successfully");
-    }
+  @MultipartMapping("/boxes")
+  public ResponseEntity<String> createBox(@ModelAttribute BoxCreation creation) {
+    s3Service.uploads(creation.files());
+//    boxService.saveBox(boxDto);
+    return ResponseEntity.ok("Box created successfully");
+  }
 }
