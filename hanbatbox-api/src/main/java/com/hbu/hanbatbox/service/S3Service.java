c63eb675d64900ab480bc8859bdab4b0a7be7e86
west_east <west.east1832@gmail.com>
2024-10-17 14:17:34 +0900

    [fix]: fix file download

--- hanbatbox-api/src/main/java/com/hbu/hanbatbox/service/S3Service.java
+++ hanbatbox-api/src/main/java/com/hbu/hanbatbox/service/S3Service.java
@@ -1,10 +1,21 @@
 package com.hbu.hanbatbox.service;
 
+import com.hbu.hanbatbox.domain.Box;
+import com.hbu.hanbatbox.repository.BoxRepository;
+import com.hbu.hanbatbox.domain.Item;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,16 +27,15 @@ import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
+@RequiredArgsConstructor
 public class S3Service {
 
   private final S3Client s3Client;
+  private final PasswordEncoder passwordEncoder;
+  private final BoxRepository boxRepository;
 
   @Value("${spring.cloud.aws.S3.bucket}")
   private String bucketName;
-
-  public S3Service(S3Client s3Client) {
-    this.s3Client = s3Client;
-  }
 
   private PutObjectRequest buildPutObjectRequest(String objectKey, MultipartFile file) {
     return PutObjectRequest.builder()
@@ -83,4 +93,81 @@ public class S3Service {
     }
   }
 
+  public boolean validatePassword(Long id, String inputPassword) {
+    Box box = boxRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Box not found"));
+
+    if (box.isCrypted()) {
+      return passwordEncoder.matches(inputPassword, box.getPassword());
+    }
+    return true;
+  }
+
+  public String[] getObjectKeysByBoxId(Long id) {
+    Box box = boxRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Box not found"));
+    return box.getItems().stream().map(Item::getObjectKey).toArray(String[]::new);
+  }
+
+  public String getBoxTitleById(Long id) {
+    return boxRepository.findById(id)
+        .map(Box::getTitle)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Box not found"));
+  }
+
+  public void downloadSingleFile(String title, String objectKey, HttpServletResponse response)
+      throws IOException {
+    InputStream inputStream = downloads(objectKey);
+    long fileSize = getFileSize(objectKey);
+
+    response.setContentType("application/octet-stream");
+    response.setContentLengthLong(fileSize);
+    response.setHeader("Content-Disposition",
+        "attachment; filename=\"" + URLEncoder.encode(title, StandardCharsets.UTF_8) + "\";");
+    response.setHeader("Content-Transfer-Encoding", "binary");
+
+    try (OutputStream outputStream = response.getOutputStream()) {
+      byte[] buffer = new byte[4096];
+      int bytesRead;
+
+      while ((bytesRead = inputStream.read(buffer)) != -1) {
+        outputStream.write(buffer, 0, bytesRead);
+      }
+
+      outputStream.flush();
+    }
+
+    inputStream.close();
+  }
+
+  public void downloadMultipleFiles(String title, String[] objectKeys, HttpServletResponse response)
+      throws IOException {
+
+    long fileSize = 0;
+    response.setContentType("application/octet-stream");
+    response.setHeader("Content-Disposition",
+        "attachment; filename=\"" + URLEncoder.encode(title, StandardCharsets.UTF_8) + "\";");
+    response.setHeader("Content-Transfer-Encoding", "binary");
+
+    try (ZipOutputStream zipOut = new ZipOutputStream(response.getOutputStream())) {
+      for (String objectKey : objectKeys) {
+        InputStream inputStream = downloads(objectKey);
+
+        ZipEntry zipEntry = new ZipEntry(objectKey);
+        zipOut.putNextEntry(zipEntry);
+        fileSize += zipEntry.getCompressedSize();
+
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+          zipOut.write(buffer, 0, bytesRead);
+        }
+
+        zipOut.closeEntry();
+        inputStream.close();
+      }
+    }
+
+    response.setContentLengthLong(fileSize);
+  }
 }
