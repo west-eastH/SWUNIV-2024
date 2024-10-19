78982a0e3556fc786ef7163c20a52643f2c02b04
galaxy4276 <deveungi@gmail.com>
2024-10-19 11:29:10 +0900

    [feat]: add api client

---
+++ app/src/shared/config/axios/index.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_ENDPOINT,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 5000,
+});
+
+apiClient.interceptors.response.use((res) => {
+  if (res.data?.data) {
+    return res.data.data;
+  }
+
+  return res.data;
+});
