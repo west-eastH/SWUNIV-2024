b3411291a78b21285b6e386fe8f4ec2c6c4c23c9
galaxy4276 <deveungi@gmail.com>
2024-10-19 11:28:33 +0900

    [chore]: add env

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_ENDPOINT: string;
+  // readonly VITE_APP_MODE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
