cb8d807ba039a15a7353b74a604f79f12cd5b9d6
galaxy4276 <deveungi@gmail.com>
2024-10-23 17:31:24 +0900

    feat: add ScrollEndObserver

---
+++ app/src/features/ScrollEndObserver/index.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode, useEffect, useRef } from 'react';
+
+type Props = {
+  children: ReactNode;
+  onScrollEnd: () => void;
+};
+
+const ScrollEndObserver: React.FC<Props> = ({ children, onScrollEnd }) => {
+  const root = useRef<HTMLDivElement>(null);
+  const anchor = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!anchor.current) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (!entry.isIntersecting) return;
+        console.log('intersecting!');
+        onScrollEnd();
+      },
+      {
+        root: root.current,
+      },
+    );
+
+    observer.observe(anchor.current);
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div ref={root} className="h-full w-full ObserverRoot">
+      {children}
+      <div id="scrollAnchor" className="w-full h-[1px]" ref={anchor} />
+    </div>
+  );
+};
+
+export default ScrollEndObserver;
