c807be5a0e8631303bafeb16510e69ec5a61b6d9
west_east <west.east1832@gmail.com>
2024-10-28 20:22:50 +0900

    [feat]: 유효성 검사 추

--- hanbatbox-api/src/main/java/com/hbu/hanbatbox/dto/BoxSaveDto.java
+++ hanbatbox-api/src/main/java/com/hbu/hanbatbox/dto/BoxSaveDto.java
@@ -1,5 +1,7 @@
 package com.hbu.hanbatbox.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +11,9 @@ public class BoxSaveDto {
 
     private String uploader;
     private String title;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 4, message = "비밀번호는 최소 4자 이상이어야 합니다.")
     private String password;
 }
 
