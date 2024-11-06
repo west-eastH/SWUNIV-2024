46af93dbc39e9d6cd1dcbbf0bf366a4c31a7a780
galaxy4276 <deveungi@gmail.com>
2024-11-07 00:06:19 +0900

    [fix]: fix do not load more data when access home

--- app/src/pages/home/ui/page.tsx
+++ app/src/pages/home/ui/page.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { Mobile } from '@features/layout';
 import DashboardPanel from '@widgets/dashboard-panel';
 import { Search, UploadBoxList, useBoxesQuery } from '@features/upload-box';
+import { Typo } from '@shared/ui';
 
 export const Home: React.FC = () => {
   const { data } = useBoxesQuery();
 
+  if (data?.boxes.length === 0) {
+    return (
+      <div className="col items-center">
+        <Typo size={14}>데이터가 존재하지 않습니다.</Typo>
+      </div>
+    );
+  }
   if (!data) return null;
 
   return (
