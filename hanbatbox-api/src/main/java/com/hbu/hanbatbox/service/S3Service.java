8ca4387ec7832cd029421f5a761420eda6a718f7
mjmj1 <mjong3365@gmail.com>
2024-11-07 09:55:54 +0900

    [fix]: file name split 수정

--- hanbatbox-api/src/main/java/com/hbu/hanbatbox/service/S3Service.java
+++ hanbatbox-api/src/main/java/com/hbu/hanbatbox/service/S3Service.java
@@ -7,6 +7,7 @@ import com.hbu.hanbatbox.repository.BoxRepository;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -52,7 +53,7 @@ public class S3Service {
       throw new IllegalArgumentException("올바른 파일을 추가해 주세요.");
     }
 
-    String extension = originFileName.split("\\.")[1];
+    String extension = Arrays.stream(originFileName.split("\\.")).toList().getLast();
     return "%d-%s.%s".formatted(System.currentTimeMillis(), title, extension);
   }
 
