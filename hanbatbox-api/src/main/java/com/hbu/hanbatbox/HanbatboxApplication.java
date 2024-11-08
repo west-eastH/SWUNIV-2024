d87ded7513cf98e9f7375c0db66ce73b9723dfdf
west_east <west.east1832@gmail.com>
2024-11-08 18:53:23 +0900

    [feat]: 타임로그 추가

--- hanbatbox-api/src/main/java/com/hbu/hanbatbox/HanbatboxApplication.java
+++ hanbatbox-api/src/main/java/com/hbu/hanbatbox/HanbatboxApplication.java
@@ -2,8 +2,10 @@ package com.hbu.hanbatbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class HanbatboxApplication {
   public static void main(String[] args) {
     SpringApplication.run(HanbatboxApplication.class, args);
