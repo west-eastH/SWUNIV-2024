d535f344911e016f5cce2bee14b938b0a09c7062
galaxy4276 <deveungi@gmail.com>
2024-11-09 13:08:33 +0900

    [chore]: add server Time Zone as  "Asia/Seoul"

--- hanbatbox-api/src/main/java/com/hbu/hanbatbox/HanbatboxApplication.java
+++ hanbatbox-api/src/main/java/com/hbu/hanbatbox/HanbatboxApplication.java
@@ -1,13 +1,22 @@
 package com.hbu.hanbatbox;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAspectJAutoProxy
 public class HanbatboxApplication {
+
   public static void main(String[] args) {
     SpringApplication.run(HanbatboxApplication.class, args);
+  }
+
+  @PostConstruct
+  public void changeTimezone() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
   }
 }
