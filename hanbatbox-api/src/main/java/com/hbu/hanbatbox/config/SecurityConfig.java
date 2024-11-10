dc0854d9ecfb883a85447fbdef6b888c50f931db
west_east <west.east1832@gmail.com>
2024-11-10 21:39:03 +0900

    [feat]: 허용 도메인 수정

--- hanbatbox-api/src/main/java/com/hbu/hanbatbox/config/SecurityConfig.java
+++ hanbatbox-api/src/main/java/com/hbu/hanbatbox/config/SecurityConfig.java
@@ -15,43 +15,45 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class SecurityConfig {
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(csrf -> csrf.disable())
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-        .authorizeHttpRequests(authz -> authz
-            .requestMatchers(new IpAddressMatcher("223.194.160.0/24")).permitAll()
-            .requestMatchers(new IpAddressMatcher("0:0:0:0:0:0:0:1")).permitAll()
-            .requestMatchers(new IpAddressMatcher("127.0.0.1")).permitAll()
-            .requestMatchers(new IpAddressMatcher("::1")).permitAll()
-            .requestMatchers(new IpAddressMatcher("125.242.221.200")).permitAll()
-            .requestMatchers(new IpAddressMatcher("175.119.50.176")).permitAll()
-            .requestMatchers(new IpAddressMatcher("222.114.188.183")).permitAll()
-            .requestMatchers(new IpAddressMatcher("124.53.118.139")).permitAll()
-            .anyRequest().permitAll()
-        )
-        .formLogin(form -> form.disable());
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers(new IpAddressMatcher("223.194.160.0/24")).permitAll()
+                .requestMatchers(new IpAddressMatcher("0:0:0:0:0:0:0:1")).permitAll()
+                .requestMatchers(new IpAddressMatcher("127.0.0.1")).permitAll()
+                .requestMatchers(new IpAddressMatcher("::1")).permitAll()
+                .requestMatchers(new IpAddressMatcher("125.242.221.200")).permitAll()
+                .requestMatchers(new IpAddressMatcher("175.119.50.176")).permitAll()
+                .requestMatchers(new IpAddressMatcher("222.114.188.183")).permitAll()
+                .requestMatchers(new IpAddressMatcher("124.53.118.139")).permitAll()
+                .anyRequest().permitAll()
+            )
+            .formLogin(form -> form.disable());
 
-    return http.build();
-  }
+        return http.build();
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://galaxy4276.asuscomm.com:3000", "http://localhost:3000"));
-    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    configuration.setAllowedHeaders(List.of("*"));
-    configuration.setExposedHeaders(List.of("Content-Disposition", "Content-Type"));
-    configuration.setAllowCredentials(true);
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(
+            List.of("https://www.han-box.co.kr/", "https://han-box.co.kr/",
+                "http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Content-Disposition", "Content-Type"));
+        configuration.setAllowCredentials(true);
 
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
